333b2a4276136a9bfcd3b0073c3bd921f039b026
Andres Vazquez <andres.vazquez.martin@gmail.com>
2023-01-20 02:22:31 +0100

    Updates

---
+++ precache-manifest.4c91b6e406a37db248f4364ad0d05639.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "8601a5d6abf01aaf6a61",
+    "url": "/static/css/main.9778e1e7.chunk.css"
+  },
+  {
+    "revision": "8601a5d6abf01aaf6a61",
+    "url": "/static/js/main.8601a5d6.chunk.js"
+  },
+  {
+    "revision": "61f4cd0be057bb88e1fb",
+    "url": "/static/js/1.61f4cd0b.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ab63d5d32577d72f211d14387164e491",
+    "url": "/static/media/CV.ab63d5d3.pdf"
+  },
+  {
+    "revision": "ac37ad427c1ec11f5fab65010699cfce",
+    "url": "/static/media/profile picture.ac37ad42.jfif"
+  },
+  {
+    "revision": "d1859e046933dce3ad9fb0376fc2c901",
+    "url": "/static/media/gafas.d1859e04.jfif"
+  },
+  {
+    "revision": "9f02a72fcff84c8c17f53faff1973272",
+    "url": "/static/media/about me.9f02a72f.jpg"
+  },
+  {
+    "revision": "627baaa884b54e3221d58072a38c2268",
+    "url": "/static/media/hello pilot.627baaa8.png"
+  },
+  {
+    "revision": "2851cc9503b6f3e49af589cc63537b5a",
+    "url": "/static/media/WhoWon.2851cc95.jpg"
+  },
+  {
+    "revision": "7d32ecb5cfecc807d88404ac10c79338",
+    "url": "/static/media/ironpump.7d32ecb5.png"
+  },
+  {
+    "revision": "68ce9485638fc70b5a99355505afef19",
+    "url": "/index.html"
+  }
+];
